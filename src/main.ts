822ae64af7fc497e1f6c51fb82bbbf84ee8e73ff
xrrac42 <carloseduardobo@gmail.com>
2024-09-16 20:33:59 -0300

    implementacao HMR no projeto

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { Routes, provideRouter } from '@angular/router';
 import { FavoritesComponent } from './features/favorites/favorites.component';
@@ -8,16 +7,33 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { BookDetailComponent } from './features/book-detail/book-detail.component';
 
 const routes: Routes = [
-  {path: '', component: HomeComponent},
-  {path: 'favorites', component: FavoritesComponent},
+  { path: '', component: HomeComponent },
+  { path: 'favorites', component: FavoritesComponent },
   { path: 'book/:id', component: BookDetailComponent },
-  { path: '**', redirectTo: '/home' } 
-
+  { path: '**', redirectTo: '' }
 ];
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes),
-    provideHttpClient(withInterceptorsFromDi())
 
-  ]
-});
+const bootstrap = () => {
+  bootstrapApplication(AppComponent, {
+    providers: [
+      provideRouter(routes),
+      provideHttpClient(withInterceptorsFromDi())
+    ]
+  }).catch(err => console.error(err));
+};
+
+declare const module: {
+  hot?: {
+    accept: () => void;
+    dispose: (callback: () => void) => void;
+  };
+};
+
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => {
+    window.location.reload();
+  });
+} else {
+  bootstrap();
+}
